42bca81ab2cbc49561f7bd956925adf6913edf6d
rupsa24092005-stack <rupsa24092005@gmail.com>
2026-02-09 15:59:30 +0530

    Add files via upload

---
+++ weekly3.py
@@ -0,0 +1,30 @@
+# assignment 1 -  Print numbers from 1 to 10.
+for i in range(1, 11):
+    Print(i)
+
+#assignment 2 -Find the factorial of a number
+#num = int(input("Enter a number: "))
+
+#fact = 1
+#for i in range(1, num + 1):
+ #   fact *= i
+
+#print("Factorial =", fact)
+
+#assignment 3-  Print multiplication table of a number
+
+#num = int(input("Enter a number: "))
+
+#for i in range(1, 11):
+ #   print(f"{num} x {i} = {num * i}")
+
+#assignmemt 4- Check whether a number is prime
+
+#num = int(input("Enter your number:- "))
+
+#if num/2 == 0 :
+ #   print("Your number is prime. ")
+#else:
+ #   print("ypur number is not prime. ")
+
+# assignment 5-  Generate Fibonacci series up to N terms.
